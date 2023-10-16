1e0b390441473341bd35b32a7db703754502ad43
PauloK3tchup <passandoalimpo13@gmail.com>
2023-10-16 11:07:52 -0300

    Banco de dados online

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -6,7 +6,7 @@ const MY_IP = import.meta.env.VITE_MY_IP
 console.log(`${MY_IP}`)
 
 const api = axios.create({
-  baseURL: `http://${MY_IP}:19003/`
+  baseURL: 'https://spamton-store-backend-dev-qzhk.1.us-1.fl0.io/'
   // baseURL: 'http://192.168.0.17:19003/api/',
 })
 
